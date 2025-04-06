44657562fc5b251587be0fe48026e4527cf4224f
dennkaii <githubdennkaii.q3i49@simplelogin.com>
2025-04-06 14:27:09 -0400

    to show in discord,nothing important

--- main.py
+++ main.py
@@ -1,20 +1,14 @@
-# Install dependencies as needed:
-# pip install kagglehub[pandas-datasets]
-import kagglehub
-from kagglehub import KaggleDatasetAdapter
+# ~/.kaggle/kaggle.json
 
-# Set the path to the file you'd like to load
-file_path = ""
+import pandas as pd
+import numpy as np
+import kaggle
 
-# Load the latest version
-df = kagglehub.load_dataset(
-    KaggleDatasetAdapter.PANDAS,
-    "amansingh0000000/smartphones",
-    file_path,
-    # Provide any additional arguments like
-    # sql_query or pandas_kwargs. See the
-    # documenation for more information:
-    # https://github.com/Kaggle/kagglehub/blob/main/README.md#kaggledatasetadapterpandas
-)
+kaggle.api.authenticate()
+kaggle.api.dataset_download_files("amansingh0000000/smartphones", path=".", unzip=True)
 
-print("First 5 records:", df.head())
+
+# read donwloaded csv
+
+df = pd.read_csv("./mobile_phones_2000.csv")
+df.head()
